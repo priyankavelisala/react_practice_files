6fb6d2abf32f4e6ab636fb870e22d40d899a547c
priyanka <priyankavelisala30@gmail.com>
2023-07-30 15:03:38 +0530

    added

---
+++ reactday2/file1.mjs
@@ -0,0 +1,9 @@
+import f3,{f1,f2} from "./file2.mjs"
+
+function f4(){
+    f1()
+    f2()
+    f3()
+}
+
+f4()
---
+++ reactday2/file2.mjs
@@ -0,0 +1,14 @@
+export function f1(){
+    console.log("f1 is  defined")
+}
+
+export function f2(){
+    console.log("f2 is defined")
+}
+
+
+function f3(){
+    console.log("f3 is defined")
+}
+
+export default f3;
